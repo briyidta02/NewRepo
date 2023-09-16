c91b5a2b0a3ad2f827c35bbabccc95fd57201156
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-16 11:58:56 -0500

    comment

---
+++ ConsoleApp2/Conductor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+        public class Conductor
+        {
+            public string Nombre { get; set; }
+            public int Telefono { get; set; }
+            public string Direccion { get; set; }
+            public int identificacion { get; set; }
+
+}
+}
