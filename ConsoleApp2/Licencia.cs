6067d39737685c7ec036ebfc25cf162a8045fcce
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-16 12:21:32 -0500

    comment

---
+++ ConsoleApp2/Licencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Licencia
+    {
+        public class Licencia
+        {
+        public string NombreConductor { get; set; }
+        public int IdentificacionPersona { get; set; }
+
+        public DateTime FechaVigencia  { get; set; }
+        public DateTime FechaExpediente { get; set; }
+        public string ClaseVehiculo { get; set; }
+            public string Servicio { get; set; }
+
+
+        }
+    }
+}
