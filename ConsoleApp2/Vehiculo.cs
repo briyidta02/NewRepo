db0ca13a537617580d049e0c8ec160a6b142c83c
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-16 12:03:57 -0500

    comment

--- ConsoleApp2/Vehiculo.cs
+++ ConsoleApp2/Vehiculo.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    internal class Vehiculo
-    {
+ 
+        public class Vehiculo
+        {
+            public string Marca { get; set; }
+            public int AniLanzamiento { get; set; }
+            public string Velocidad { get; set; }
+            public string Color { get; set; }
+            public string Tipo { get; set; }
+            public int Modelo { get; set; }
+
+
+
+
     }
 }
