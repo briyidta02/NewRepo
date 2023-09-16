14b3b9fc0311bfecf2cace30c9691a964a9a6802
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-16 12:14:43 -0500

    comment

---
+++ ConsoleApp2/Mantenimiento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Mantenimiento
+    {
+        public class mantenimiento
+        {
+            public DateTime FechaInicio { get; set; }
+            public string MarcaAceite { get; set; }
+            public string Servicio { get; set; }
+            public double Kilometraje { get; set; }
+            public double ProximoCambio { get; set; }
+            public double Costo { get; set; }
+            public double Lineamientos { get; set; }
+            public string NombreTaller { get; set; }
+            public string NombreMecanico { get; set; }
+            public string Frenos { get; set; }
+
+        }
+}
+}
