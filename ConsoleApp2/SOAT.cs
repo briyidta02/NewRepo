6cd482df399e6e9f80a51f12cf0fa7672790ba5c
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-16 12:07:27 -0500

    comment

---
+++ ConsoleApp2/SOAT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class SOAT
+    {
+        public class Soat
+        {
+            public DateTime  FechaFinal { get; set; }
+        public DateTime FechaProxima { get; set; }
+        public int NumeroSoat { get; set; }
+
+
+        }
+
+
+
+    }
+    }
+
