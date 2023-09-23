de77652a88eef430555da54de81de887cefc768e
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-23 11:56:12 -0500

    commet

--- ConsoleApp2/Conductor.cs
+++ ConsoleApp2/Conductor.cs
@@ -8,10 +8,56 @@ namespace ConsoleApp2
 {
         public class Conductor
         {
-            private string Nombre { get; set; }
-            private int Telefono { get; set; }
-            private string Direccion { get; set; }
-            private int identificacion { get; set; }
+        private string _Nombre;
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+            set
+            {
+                _Nombre = value;
+            }
+        }
+        private int _Telefono;
+        public int Telefono
+        {
+            get
+            {
+                return _Telefono;
+            }
+            set
+            {
+                _Telefono = value;
+            }
+        }
+        private string _Direccion;
+        public string Direccion
+        {
+            get
+            {
+                return _Direccion;
+            }
+            set
+            {
+                _Direccion = value;
+            }
+        }
 
-}
+
+        private int _identificacion;
+
+        public int Identificacion
+        {
+            get
+            {
+                return _identificacion;
+            }
+            set
+            {
+                _identificacion = value;
+            }
+        }
+    }
 }
