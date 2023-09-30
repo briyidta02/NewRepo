19dd779b792ca19af633b0b5713cc0861dd1043d
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-09-30 12:07:44 -0500

    comment

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
             Vehiculo vehiculo = new Vehiculo("BMW", 0, "media", "Rojo", "Deportivo", 2018);
-            Console.WriteLine(vehiculo.Marca);
+            Console.WriteLine("la marca del carro es:" + vehiculo.Marca);
+            Console.WriteLine("Su alineacion es :"+vehiculo.alineamiento);
+            Console.WriteLine("Su nivel de velozidad es: " + vehiculo.Velocidad);
+            Console.WriteLine("el color es: " + vehiculo.color);
+            Console.WriteLine("El tipo de carro es:" + vehiculo.Tipo);
+            Console.WriteLine("el modelo del carro es del ano: " + vehiculo.Modelo);
         }
     }
 }
