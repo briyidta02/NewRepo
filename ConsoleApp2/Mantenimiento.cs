35e39a646515d3c6bde975d27b945f700d527367
Briyid Tatiana Bautista Atuesta <briyidt.bautistaa@konradlorenz.edu.co>
2023-10-13 21:30:14 -0600

    commet

--- ConsoleApp2/Mantenimiento.cs
+++ ConsoleApp2/Mantenimiento.cs
@@ -23,17 +23,114 @@ namespace ConsoleApp2
                 }
             }
 
-            private string MarcaAceite { get; set; }
-
-            private string Servicio { get; set; }
-            private double Kilometraje { get; set; }
-            private double ProximoCambio { get; set; }
-            private double Costo { get; set; }
-            private double Lineamientos { get; set; }
-            private string NombreTaller { get; set; }
-            private string NombreMecanico { get; set; }
-            private string Frenos { get; set; }
-
+            private string _Marcasceite { get; set; }
+            public string MarcaAceite 
+            {
+                get
+                {
+                    return _Marcasceite;
+                }
+                set
+                {
+                    _Marcasceite  = value;
+                }
+            }
+            private string _Servicio { get; set; }
+            public string Servicio
+            {
+                get
+                {
+                    return _Servicio;
+                }
+                set
+                {
+                    _Servicio = value;
+                }
+            }
+            private double _Kilometraje { get; set; }
+            public double Kilometraje
+            {
+                get
+                {
+                    return _Kilometraje;
+                }
+                set
+                {
+                    _Kilometraje = value;
+                }
+            }
+            private double _Proximocambio { get; set; }
+            public double ProximoCambio
+            {
+                get
+                {
+                    return _Proximocambio;
+                }
+                set
+                {
+                    _Proximocambio = value;
+                }
+            }
+            private double _Costo { get; set; }
+            public double Costo
+            {
+                get
+                {
+                    return _Costo;
+                }
+                set
+                {
+                    _Costo = value;
+                }
+            }
+            private double _Lineamientos { get; set; }
+            public double Lineamiento
+            {
+                get
+                {
+                    return _Lineamientos;
+                }
+                set
+                {
+                    _Lineamientos = value;
+                }
+            }
+            private string _Nombretaller { get; set; }
+            public string NombreTaller
+            {
+                get
+                {
+                    return _Nombretaller;
+                }
+                set
+                {
+                    _Nombretaller = value;
+                }
+            }
+            private string _Nombremecanico { get; set; }
+            public string NombreMecanico
+            {
+                get
+                {
+                    return _Nombremecanico;
+                }
+                set
+                {
+                    _Nombremecanico = value;
+                }
+            }
+            private string _Frenos { get; set; }
+            public string Frenos
+            {
+                get
+                {
+                    return _Frenos;
+                }
+                set
+                {
+                    _Frenos= value;
+                }
+            }
         }
 }
 }
