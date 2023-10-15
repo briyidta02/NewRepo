6a5a69bb192e01d8600bbca97b68ae5a4f9ce4b1
Briyid Tatiana Bautista Atuesta <briyidt.bautistaa@konradlorenz.edu.co>
2023-10-15 09:58:08 -0600

    comment

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("INFORMACION VEHICULO");
+            Console.WriteLine("**INFORMACION VEHICULO**");
             Vehiculo vehiculo = new Vehiculo("BMW", 0, "media", "Rojo", "Deportivo", 2018);
             Console.WriteLine("la marca del carro es:" + vehiculo.Marca);
             Console.WriteLine("Su alineacion es :" + vehiculo.alineamiento);
@@ -13,13 +13,13 @@
             Console.WriteLine("El tipo de carro es:" + vehiculo.Tipo);
             Console.WriteLine("el modelo del carro es del ano es: " + vehiculo.Modelo);
 
-            Console.WriteLine("INFORMACION SOAT");
+            Console.WriteLine("**INFORMACION SOAT**");
             Soat SOAT = new Soat(198876, "09-05-2021", "09-05-2022");
             Console.WriteLine("El numero de soat es:" + SOAT.NumeroSoat);
             Console.WriteLine("la fecha proxima es:" + SOAT.FechaProxima);
             Console.WriteLine("La fecha final es de: " + SOAT.FechaFinal);
 
-            Console.WriteLine("INFORMACION LICENCIA");
+            Console.WriteLine("**INFORMACION LICENCIA**");
             Licencia licencia = new Licencia(3525666, "02-08-2024", "02-08-2021", "Particular", "Antonella Ramirez");
             Console.WriteLine("Numero de licencia es:" + licencia.IdentificacionPersona);
             Console.WriteLine("Fehca de vigencia es: " + licencia.FechaVigencia);
@@ -27,9 +27,12 @@
             Console.WriteLine("Servicio:" + licencia.Servicio);
             Console.WriteLine("Nombre de conductor: " + licencia.NombreConductor);
 
-            Console.WriteLine("INFORMACION CONDUCTOR");
-            Conductor conductor = new Conductor("Antonella Ramirez", 3152023879, "calle 54 G N 23 - 25", 1019958263);
-
+            Console.WriteLine("**INFORMACION CONDUCTOR**");
+            Conductor conductor = new Conductor("Antonella Ramirez",315-202-3879,"calle 54 G N 23 - 25", 1019958263);
+            Console.WriteLine("Nombre de conductor:" + conductor.Nombre);
+            Console.WriteLine("Telefono del conductor: " + conductor.Telefono);
+            Console.WriteLine("Direccion del conductor:" + conductor.Direccion);
+            Console.WriteLine("Numero de identificacion:" + conductor.Identificacion);
 
         }
     }
