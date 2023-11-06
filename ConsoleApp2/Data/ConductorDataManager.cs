52aef5fc30bce401ea90fead7e6e42597dde5787
Briyid Tatiana Bautista Atuesta <briyidt.bautistaa@konradlorenz.edu.co>
2023-11-06 00:59:19 -0600

    comment

--- ConsoleApp2/Data/ConductorDataManager.cs
+++ ConsoleApp2/Data/ConductorDataManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +10,39 @@ namespace ConsoleApp2.Data
 {
     public static class ConductorDataManager
     {
-        private const string DATA_FILE = "";
+        private const string DATA_FILE = "C:\\Users\\Tatiana\\Source\\Repos\\briyidta02\\NewRepo\\ConsoleApp2\\Data\\Conductor.json";
         public static Conductor AddConductor(Conductor conductor)
         {
+            try
+            {
+                string currentConductorState = GetFileInfo();
+                var jObject = JObject.Parse(currentConductorState);
+                conductor.Id = $"{jObject.Properties().Count() +1}";
+                string ConductorJson = JsonConvert.SerializeObject(conductor);
+                jObject.Add(conductor.Id, ConductorJson);
+                
+                string outoutJson = JsonConvert.SerializeObject(jObject, Formatting.Indented);
+                WriteFileInfo(outoutJson);
+            }
+            catch (Exception ex) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.Green;
+            
+            
+            }
+
             return conductor;
         }
-    
+        private static string GetFileInfo()
+        {
+
+            return File.ReadAllText(DATA_FILE);
+        }
+        private static void WriteFileInfo(string json)
+        {
+            File.WriteAllText(DATA_FILE, json);
+        }
     }
 }
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace ConsoleApp2
+﻿using ConsoleApp2.Data;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             DateTime fechageneral = new DateTime(2023, 10, 21);
-            
+
             Console.WriteLine("**INFORMACION VEHICULO**");
             Vehiculo vehiculo = new Vehiculo("BMW", 0, "media", "Rojo", "Deportivo", 2018);
             Console.WriteLine("la marca del carro es:" + vehiculo.Marca);
@@ -22,7 +24,7 @@
             Console.WriteLine("La fecha final es de: " + SOAT.FechaFinal);
 
             Console.WriteLine("**INFORMACION LICENCIA**");
-            Licencia licencia = new Licencia(3525666, new DateTime(2024,08,02), new DateTime(2021, 08, 02), "Particular", "Antonella Ramirez");
+            Licencia licencia = new Licencia(3525666, new DateTime(2024, 08, 02), new DateTime(2021, 08, 02), "Particular", "Antonella Ramirez");
             Console.WriteLine("Numero de licencia es:" + licencia.IdentificacionPersona);
             Console.WriteLine("Fehca de vigencia es: " + licencia.FechaVigencia);
             Console.WriteLine("Fecha de expediente: " + licencia.FechaExpediente);
@@ -30,11 +32,13 @@
             Console.WriteLine("Nombre de conductor: " + licencia.NombreConductor);
 
             Console.WriteLine("**INFORMACION CONDUCTOR**");
-            Conductor conductor = new Conductor("Antonella Ramirez",315-202-3879,"calle 54 G N 23 - 25", 1019958263);
+            Conductor conductor = new Conductor("Antonella Ramirez", 315 - 202 - 3879, "calle 54 G N 23 - 25", 1019958263);
             Console.WriteLine("Nombre de conductor:" + conductor.Nombre);
             Console.WriteLine("Telefono del conductor: " + conductor.Telefono);
             Console.WriteLine("Direccion del conductor:" + conductor.Direccion);
             Console.WriteLine("Numero de identificacion:" + conductor.Identificacion);
+
+            ConductorDataManager.AddConductor(conductor);
 
             Console.WriteLine("**INFORMACION MANTENIMIENTO**");
             Mantenimiento Mantenimiento = new Mantenimiento(new DateTime(2022, 06, 02),"Formula 1","mantenimiento general",160648,02-12-2022,105.000,0.5,"MECATRONIX","Richar Monsalve","Equilibrados");
