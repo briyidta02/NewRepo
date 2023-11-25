9606507dc25be3cb4ad1dbc2c4034bbc60896901
Briyid Tatiana Bautista Atuesta <briyidt.bautistaa@konradlorenz.edu.co>
2023-11-24 22:39:08 -0600

    comment

--- ConsoleApp2/HojadeVida.cs
+++ ConsoleApp2/HojadeVida.cs
@@ -8,11 +8,69 @@ namespace ConsoleApp2
 {
     internal class HojadeVida
     {
+       
         public class Hojadevida
         {
             private string mantenimiento { get; set; }
+
+        }
+        private static int opcion;
+        static void Main(string[] args)
+        {
+            MostrarMenu();
         }
 
+        public static void MostrarMenu()
+        {
 
-}
-}
+            Console.WriteLine("MENÚ");
+            Console.WriteLine("1. RegistrarRevision");
+            Console.WriteLine("2. MostrarRevisiones Programadas");
+            Console.WriteLine("3. EMPEZAR DE NUEVO");
+
+            while (true)
+            {
+                MostrarMenu();
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        RegistrarRevision();
+                        break;
+                    case "2":
+                        MostrarRevisionesProgramadas();
+                        break;
+                    case "4":
+                        return;
+
+                }
+
+            }
+        }
+                public static void RegistrarRevision()
+                {
+                    Console.Write("Ingrese la descripción de la revisión: ");
+                    string descripcion = Console.ReadLine();
+
+                    Console.Write("Ingrese la fecha de la revisión (YYYY-MM-DD): ");
+                    DateTime fecha = DateTime.Parse(Console.ReadLine());
+
+                    Revision nuevaRevision = new Revision(descripcion, fecha);
+                    revisiones.Add(nuevaRevision);
+
+                    Console.WriteLine("Revisión registrada con éxito.");
+                }
+
+                public static void MostrarRevisionesProgramadas()
+                {
+                    Console.WriteLine("Revisiones programadas:");
+                    foreach (var revision in revisiones)
+                    {
+                        Console.WriteLine($"{revision.Descripcion} - {revision.Fecha}");
+                    }
+                }
+            }
+        }
+
+    } }
---
+++ ConsoleApp2/Revision.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2
+{
+  public class Revision
+    {
+        private string _Descripcionvehiculo;
+        public string DescripcionVehiculo
+        {
+            get
+            {
+                return _Descripcionvehiculo;
+            }
+            set
+            {
+              _Descripcionvehiculo = value;
+            }
+        }
+        private string _Registrorevision;
+        public string RegistroRevision
+        {
+            get
+            {
+                return _Registrorevision;
+            }
+            set
+            {
+                _Registrorevision  = value;
+            }
+        }
+    }
+}
